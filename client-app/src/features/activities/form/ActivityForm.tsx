1d331fa96e8fd2894666880931859c55bf59dbf9
Zee <zebanaya@protonmail.com>
2023-01-25 13:51:10 -0800

    End of Section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,7 +12,7 @@ import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/CategoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
-import { Activity } from "../../../app/models/Activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 
 export default observer(function ActivityForm() {
     const history = useHistory();
@@ -20,15 +20,7 @@ export default observer(function ActivityForm() {
     const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams<{id: string}>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '', 
-        venue: ''
-    });    
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues())
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -40,16 +32,16 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
             };
-            createActivity(newActivity).then(() => history.push(`/activities/${activity.id}`))
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`))
         } else {
             updateActivity(activity).then(() => history.push(`/activities/${activity.id}`))
         }
@@ -81,7 +73,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button 
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} 
+                            loading={isSubmitting} 
                             floated='right' 
                             type='submit' 
                             content='Submit' 
